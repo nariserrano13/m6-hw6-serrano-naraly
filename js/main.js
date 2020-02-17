1965759b857e8363218c36b59016563a9621713c
Naraly Serrano <naralycserrano@ufl.edu>
2020-02-17 11:37:54 -0500

    Append text node to subheading class

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var squares = document.getElementsByClassName('square');
 
-
 function changeBackground (){
     for(i=0; i<squares.length;i++){
         squares[i].style.backgroundColor='red';
@@ -8,8 +7,30 @@ function changeBackground (){
     
    //console.log('is this working');
 } 
-
 for(i=0; i< squares.length; i++){
     squares[i].addEventListener('mouseover',changeBackground);
     squares[i].addEventListener('mouseout',changeBackground);
+}
+
+function squareOne (){
+    var myElement = document.createElement('p');
+    var squareOneText = document.createTextNode('Ooch - so close but no cigar');
+    
+    myElement.appendChild(squareOneText);
+
+    document.getElementsByClassName('subheading')[0].appendChild(myElement);
+}
+function squareTwo(){
+    var myElement = document.createElement('p');
+    var squareTwoText = document.createTextNode('DING DING DING - We have a winner');
+
+    myElement.appendChild(squareTwoText);
+    document.getElementsByClassName('subheading')[0].appendChild(myElement);
+}
+function squareThree(){
+    var myElement = document.createElement('p');
+    var squareThreeText = document.createTextNode('Oops better luck next time');
+
+    myElement.appendChild(squareThreeText);
+    document.getElementsByClassName('subheading')[0].appendChild(myElement);
 }
