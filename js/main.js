22a460ce05836c60ad6934ea7fca500c7b754a4a
Naraly Serrano <naralycserrano@ufl.edu>
2020-02-17 17:52:28 -0500

    Remove node using js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 var squares = document.getElementsByClassName('square');
 
+//use for loop to apply js to each squares element
+//use i< squares.length to ensure that eventlistener is applying to each sqaure within the squares array length
 function changeBackground (){
     for(i=0; i<squares.length;i++){
-        squares[i].style.backgroundColor='red';
+        squares[i].style.backgroundColor='#ffa83d';
     }
-    
-   //console.log('is this working');
 } 
 for(i=0; i< squares.length; i++){
     squares[i].addEventListener('mouseover',changeBackground);
     squares[i].addEventListener('mouseout',changeBackground);
 }
 
+/*APPENDED node
+initialized var myElement and used createElement to create a <p> tag
+initialized squareOneText to have a value of createTextNode and input unique text
+associated myElement to squareOneText so the <p> had content to fill 
+designated where the node ( p tag) was going to be appended by using document.getElementsByClassName('subheading')[0]
+eventlistener onclick is in the html and will run the function squareOne() 
+*/
 function squareOne (){
     var myElement = document.createElement('p');
     var squareOneText = document.createTextNode('Ooch - so close but no cigar');
@@ -34,3 +41,30 @@ function squareThree(){
     myElement.appendChild(squareThreeText);
     document.getElementsByClassName('subheading')[0].appendChild(myElement);
 }
+
+//REMOVED A CLASS (class ='show') TO AN ELEMENT ON 'click'
+var startOver = document.getElementsByClassName('button')[0];
+
+function showButton(){
+    startOver.classList.remove('button');
+}
+
+for (i=0; i<squares.length; i++){
+    squares[i].addEventListener('click', showButton);
+}
+
+/*
+//ADDED A CLASS (class ='show') TO AN ELEMENT ON 'click'
+
+var startOver = document.getElementsByClassName('button')[0];
+
+function showButton(){
+    startOver.classList.add('show');
+}
+
+//the for loop allows me to run the eventListener on each element with the squares 
+//class. Set squares variable at top of code on line 1
+for (i=0; i<squares.length; i++){
+    squares[i].addEventListener('click', showButton);
+}
+*/
